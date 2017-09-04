5e0f77517b746e855afff79849085889ac2dfd81
Shent <shentov@gmail.com>
2017-09-04 14:17:52 +0100

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var a = function a(message) {
+    console.log(message);
+};
+
+a('Start of the project goes here...');
+
+exports.default = a;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const a = message => {
+    console.log(message)
+}
+
+a('Start of the project goes here...')
+
+export default a
